1f4a2bcb8a9dd56bb0c435abd9e200b99ca9bad1
Levi Adoptante <36682474+theleviathan75@users.noreply.github.com>
2021-02-11 21:43:54 -0500

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -54,7 +54,7 @@ btnRoll.addEventListener('click', function () {
 
     // 2. Display dice
     diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.src = `starter/dice-${dice}.png`;
 
     // 3. Check for rolled 1: if true, switch to next player
     if (dice !== 1) {

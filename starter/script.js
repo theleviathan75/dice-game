0c23946805bf2c23f3f06b4107eac3e55606dded
Levi Adoptante <36682474+theleviathan75@users.noreply.github.com>
2021-02-20 19:36:26 -0500

    changes to script

--- starter/script.js
+++ starter/script.js
@@ -1,23 +1,24 @@
-'use strict';
+"use strict";
 
 // Selecting elements
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const current0EL = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0EL = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const name0El = document.querySelector("#name--0");
+const name1El = document.querySelector("#name--1");
 
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
 let scores, currentScore, activePlayer, playing;
 
 const init = function () {
-
   scores = [0, 0];
   currentScore = 0;
   activePlayer = 0;
@@ -27,13 +28,14 @@ const init = function () {
   score1El.textContent = 0;
   current0EL.textContent = 0;
   current1El.textContent = 0;
+  name0El.textContent = "Player 1";
+  name1El.textContent = "Player 2";
 
-  diceEl.classList.add('hidden');
-  player1El.classList.remove('player--winner');
-  player0El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-
+  diceEl.classList.add("hidden");
+  player1El.classList.remove("player--winner");
+  player0El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
 };
 
 init();
@@ -42,18 +44,18 @@ const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
 };
 
 // Rolling dice functionality
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   if (playing) {
     // 1. Generate a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
 
     // 2. Display dice
-    diceEl.classList.remove('hidden');
+    diceEl.classList.remove("hidden");
     diceEl.src = `starter/dice-${dice}.png`;
 
     // 3. Check for rolled 1: if true, switch to next player
@@ -70,7 +72,7 @@ btnRoll.addEventListener('click', function () {
   }
 });
 
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener("click", function () {
   if (playing) {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore;
@@ -81,13 +83,15 @@ btnHold.addEventListener('click', function () {
     if (scores[activePlayer] >= 50) {
       // Finish the game
       playing = false;
-      diceEl.classList.add('hidden');
+      document.querySelector(`#name--${activePlayer}`).textContent =
+        "Winner!! 🥇";
+      diceEl.classList.add("hidden");
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
+        .classList.add("player--winner");
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
+        .classList.remove("player--active");
     } else {
       // Switch to next player
       switchPlayer();
@@ -95,4 +99,30 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', init);
+btnNew.addEventListener("click", init);
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelector(".btn--how");
+
+const openModal = function () {
+  modal.classList.remove("hidden2");
+  overlay.classList.remove("hidden2");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden2");
+  overlay.classList.add("hidden2");
+};
+
+btnsOpenModal.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden2")) {
+    closeModal();
+  }
+});
